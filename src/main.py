cba087f02ae6d7310eb9d9a089e7929a909027b2
mjones2453 <mjones942676@gmail.com>
2025-03-27 17:44:35 -0400

    Update main.py and source files

--- src/main.py
+++ src/main.py
@@ -1,1 +1,11 @@
-print("hello world")
+from textnode import TextNode
+
+def main():
+    print("hello world")
+
+node = TextNode("text", "link", "url")
+
+print(repr(node))
+
+if __name__ == "__main__":
+    main()
